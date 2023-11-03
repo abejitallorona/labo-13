47e49fb5b53b3210c3e3aff478c481054e1531cf
majito <mariajorm2004@gmail.com>
2023-11-03 16:57:04 -0500

    correccion

---
+++ js/utils.js
@@ -0,0 +1,10 @@
+export class Tarea {
+    description = "";
+    status = "";
+
+    constructor(description, status) {
+        this.description = description;
+        this.status = status;
+    }
+
+}
