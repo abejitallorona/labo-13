70be628eed5f808a925e8235eea1b88ebfe8cdf9
majito <mariajorm2004@gmail.com>
2023-11-06 01:13:49 -0500

    estilos y cositas finales

--- js/script.js
+++ js/script.js
@@ -1,104 +1,85 @@
-import { } from "./utils.js";
+
+import { Task, saveTask, loadTasks } from './utils.js';
 
 const render = () => {
 
-const todoSection = document.getElementById("todo");
-const doingSection = document.getElementById("doing");
-const doneSection = document.getElementById("done");
 
-const form = document.getElementById("todo-form");
-const taskInput = document.getElementById("taskInput");
-
-function createCard(text) {
-  const card = document.createElement("div");
-  card.classList.add("card");
-
-  const taskText = document.createElement("p");
-  taskText.textContent = text;
-
-  const leftButton = document.createElement("button");
-  leftButton.textContent = "<";
-  leftButton.classList.add("left-button");
-
-  const rightButton = document.createElement("button");
-  rightButton.textContent = ">";
-  rightButton.classList.add("right-button");
-
-  const deleteButton = document.createElement("button");
-  deleteButton.textContent = "X";
-  deleteButton.classList.add("delete-button");
-
-  card.appendChild(deleteButton);
-  card.appendChild(taskText);
-  card.appendChild(leftButton);
-  card.appendChild(rightButton);
-
-  todoSection.insertBefore(card, todoSection.firstChild);
-}
-
-form.addEventListener("submit", function(event) {
-  event.preventDefault();
-  const inputText = taskInput.value;
-  createCard(inputText);
-  taskInput.value = "";
-});
-
-todoSection.addEventListener("click", function(event) {
-  const target = event.target;
-  const card = target.parentNode;
-
-  if (target.classList.contains("left-button")) {
-    const previousSection = card.parentNode.previousElementSibling;
-    if (previousSection) {
-      previousSection.appendChild(card);
+    const todoSection = document.getElementById("todo");
+    const doingSection = document.getElementById("doing");
+    const doneSection = document.getElementById("done");
+    const form = document.getElementById("todo-form");
+    const taskInput = document.getElementById("taskInput");
+    
+    function createCard(task) {
+      const card = task.createCard();
+    
+      card.querySelector(".left-button").addEventListener("click", function() {
+        const previousSection = card.parentNode.previousElementSibling;
+        if (previousSection) {
+          previousSection.appendChild(card);
+          task.state = previousSection.id;
+          saveTasksToLocalStorage();
+        }
+      });
+    
+      card.querySelector(".right-button").addEventListener("click", function() {
+        const nextSection = card.parentNode.nextElementSibling;
+        if (nextSection) {
+          nextSection.insertBefore(card, nextSection.firstChild);
+          task.state = nextSection.id;
+          saveTasksToLocalStorage();
+        }
+      });
+    
+      card.querySelector(".delete-button").addEventListener("click", function() {
+        card.remove();
+        tasks = tasks.filter((t) => t !== task);
+        saveTasksToLocalStorage();
+      });
+    
+      return card;
     }
-  } else if (target.classList.contains("right-button")) {
-    const nextSection = card.parentNode.nextElementSibling;
-    if (nextSection) {
-      nextSection.appendChild(card);
+    
+    function saveTasksToLocalStorage() {
+      const tasks = Array.from(todoSection.children)
+        .concat(Array.from(doingSection.children))
+        .concat(Array.from(doneSection.children))
+        .map((card) => {
+          const text = card.querySelector("p").textContent;
+          const state = card.parentNode.id;
+          return new Task(text, state);
+        });
+    
+      localStorage.setItem("tasks", JSON.stringify(tasks));
     }
-  } else if (target.classList.contains("delete-button")) {
-    card.remove();
-  }
-});
+    
+    const tasks = loadTasks();
+    
+    tasks.forEach((task) => {
+      let section;
+      if (task.state === "To Do") {
+        section = todoSection;
+      } else if (task.state === "Doing") {
+        section = doingSection;
+      } else if (task.state === "Done") {
+        section = doneSection;
+      }
+    
+      const card = createCard(task);
+      section.insertBefore(card, section.firstChild);
+    });
+    
+    form.addEventListener("submit", function(event) {
+      event.preventDefault();
+      const inputText = taskInput.value;
+      const task = new Task(inputText);
+      const card = createCard(task);
+      todoSection.insertBefore(card, todoSection.firstChild);
+      saveTask(task);
+      taskInput.value = "";
+    });
 
-doingSection.addEventListener("click", function(event) {
-  const target = event.target;
-  const card = target.parentNode;
-
-  if (target.classList.contains("left-button")) {
-    const previousSection = card.parentNode.previousElementSibling;
-    if (previousSection) {
-      previousSection.appendChild(card);
-    }
-  } else if (target.classList.contains("right-button")) {
-    const nextSection = card.parentNode.nextElementSibling;
-    if (nextSection) {
-      nextSection.appendChild(card);
-    }
-  } else if (target.classList.contains("delete-button")) {
-    card.remove();
-  }
-});
-
-doneSection.addEventListener("click", function(event) {
-  const target = event.target;
-  const card = target.parentNode;
-
-  if (target.classList.contains("left-button")) {
-    const previousSection = card.parentNode.previousElementSibling;
-    if (previousSection) {
-      previousSection.appendChild(card);
-    }
-  } else if (target.classList.contains("right-button")) {
-    const nextSection = card.parentNode.nextElementSibling;
-    if (nextSection) {
-      nextSection.appendChild(card);
-    }
-  } else if (target.classList.contains("delete-button")) {
-    card.remove();
-  }
-});
+    
 
 }
 
--- js/utils.js
+++ js/utils.js
@@ -1,11 +1,46 @@
 export class Task {
-    description = "";
-    status = "";
-
-    constructor(description, status) {
-        this.description = description;
-        this.status = status;
+    constructor(text, state = "To Do") {
+      this.text = text;
+      this.state = state;
     }
-
-}
-
+  
+    createCard() {
+      const card = document.createElement("div");
+      card.classList.add("card");
+  
+      const taskText = document.createElement("p");
+      taskText.textContent = this.text;
+  
+      const leftButton = document.createElement("button");
+      leftButton.textContent = "<";
+      leftButton.classList.add("left-button");
+  
+      const rightButton = document.createElement("button");
+      rightButton.textContent = ">";
+      rightButton.classList.add("right-button");
+  
+      const deleteButton = document.createElement("button");
+      deleteButton.textContent = "X";
+      deleteButton.classList.add("delete-button");
+  
+      card.appendChild(deleteButton);
+      card.appendChild(taskText);
+      card.appendChild(leftButton);
+      card.appendChild(rightButton);
+  
+      return card;
+    }
+  }
+  
+  export function saveTask(task) {
+    let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    tasks.push({ text: task.text, state: task.state });
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }
+  
+  export function loadTasks() {
+    const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    return tasks.map((task) => new Task(task.text, task.state));
+  }
+  
+  
